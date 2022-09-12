768bbc3040cf029eaeca8cdc8b3ee0d4a58cee22
Maxim Kirshin <meowto16@gmail.com>
2022-09-13 01:57:43 +0500

    Prototype. Tabbar simple styles

--- src/components/Tabbar/Tabbar.tsx
+++ src/components/Tabbar/Tabbar.tsx
@@ -1,25 +1,40 @@
 import React from 'react'
+import clsx from 'clsx'
 
 import { useScreen } from '../../hooks/useScreen'
 import { Screen } from '../../interfaces/Screen'
 
+import styles from './Tabbar.module.css'
+
 export const Tabbar: React.FC = () => {
   const { activeScreen, changeScreen } = useScreen()
+  
+  const items = [
+    { screen: Screen.calendar, title: 'Calendar' },
+    { screen: Screen.goals, title: 'Goals' },
+  ]
 
   return (
-    <div>
-      <button
-        disabled={activeScreen === Screen.calendar}
-        onClick={() => changeScreen(Screen.calendar)}
-      >
-        Calendar
-      </button>
-      <button
-        disabled={activeScreen === Screen.goals}
-        onClick={() => changeScreen(Screen.goals)}
-      >
-        Goals
-      </button>
+    <div className={styles.container}>
+      <nav className={styles.tabbar}>
+        {items.map((item) => {
+          const isActive = activeScreen === item.screen
+
+          return (
+            <button
+              key={item.screen}
+              className={clsx({
+                [styles.tabbar__item]: true,
+                [styles.tabbar__item_active]: isActive
+              })}
+              disabled={isActive}
+              onClick={() => changeScreen(item.screen)}
+            >
+              {item.title}
+            </button>
+          )
+        })}
+      </nav>
     </div>
   )
 }
